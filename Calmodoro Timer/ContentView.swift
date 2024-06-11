266b6a8b0fdb153ba2d07d8702f69daa8d51900d
Jordon Bigelow <the-music-dude@hotmail.com>
2024-06-11 09:21:52 -0600

    refactored the timer into a sparate view and called the view in ContentView/Main

---
+++ Calmodoro Timer/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  SwiftDataExample
+//
+//  Created by Jordon Bigelow on 6/2/24.
+//
+
+/*
+ TODO:
+ I want the tasks to link with the timers. So esentially, if you make a
+ timer by interacting with the set timer function, and then click the plus
+ button, it will create a task that you can then touch that will set the
+ timer to whatever time is stored in that task. Essentially, this makes
+ it super easy to estimate how long a task will take you to complete, it
+ will promote the pomodoro method by causing you to chunk a large task
+ into a smaller task you can accomplish in a small amount of time, such as
+ 20-40 minutes with a 5-15 minute break afterwards.
+ */
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    // MARK: - Properties
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Task]
+    
+    // MARK: - Initializer
+    init(seconds: TimeInterval = 0) {
+        // 25 minutes in seconds = 1500
+        //pomodoroTimerViewModel = TimerViewModel(seconds: seconds, goalTime: 120)
+        // 5 minutes in seconds = 300
+        //breakTimerViewModel = TimerViewModel(seconds: seconds, goalTime: 60)
+    }
+    
+    var body: some View {
+        // MARK: - Timer View
+        VStack {
+            TimerView()
+            // MARK: - Tasks View
+            VStack {
+                NavigationSplitView {
+                    List {
+                        ForEach(items) { item in
+                            NavigationLink {
+                                Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                            } label: {
+                                Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Text("Tasks")
+                        }
+                        
+                        ToolbarItem {
+                            Button(action: addItem) {
+                                Label("Add Item", systemImage: "plus")
+                            }
+                        }
+                        
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            EditButton()
+                        }
+                    }
+                } detail: {
+                    Text("Select an item")
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - Private Methods
+    private func addItem() {
+        withAnimation {
+            let newItem = Task(timestamp: Date())
+            modelContext.insert(newItem)
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(items[index])
+            }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Task.self, inMemory: true)
+}
--- Calmodoro Timer/TimerView.swift
+++ Calmodoro Timer/TimerView.swift
@@ -1,31 +1,15 @@
 //
-//  ContentView.swift
-//  SwiftDataExample
+//  TimerView.swift
+//  Calmodoro Timer
 //
-//  Created by Jordon Bigelow on 6/2/24.
+//  Created by Jordon Bigelow on 6/11/24.
 //
-
-/*
- TODO:
- I want the tasks to link with the timers. So esentially, if you make a
- timer by interacting with the set timer function, and then click the plus
- button, it will create a task that you can then touch that will set the
- timer to whatever time is stored in that task. Essentially, this makes
- it super easy to estimate how long a task will take you to complete, it
- will promote the pomodoro method by causing you to chunk a large task
- into a smaller task you can accomplish in a small amount of time, such as
- 20-40 minutes with a 5-15 minute break afterwards.
- */
 
 import SwiftUI
-import SwiftData
 
-struct ContentView: View {
-    // MARK: - Properties
-    @Environment(\.modelContext) private var modelContext
-    @ObservedObject var pomodoroTimerViewModel: TimerViewModel
-    @ObservedObject var breakTimerViewModel: TimerViewModel
-    @Query private var items: [Task]
+struct TimerView: View {
+    @ObservedObject var pomodoroTimerViewModel: TimerViewModel = TimerViewModel(seconds: 0, goalTime: 120)
+    @ObservedObject var breakTimerViewModel: TimerViewModel = TimerViewModel(seconds: 0, goalTime: 60)
     @State var isPomodoroTimerActive = true
     @State var isBreakTimerActive = false
     @State var isSetTimerButtonActive = false
@@ -34,16 +18,7 @@ struct ContentView: View {
     @State var pomodoroTimer: Double = 0
     @State var breakTimer: Double = 0
     
-    // MARK: - Initializer
-    init(seconds: TimeInterval = 0) {
-        // 25 minutes in seconds = 1500
-        pomodoroTimerViewModel = TimerViewModel(seconds: seconds, goalTime: 120)
-        // 5 minutes in seconds = 300
-        breakTimerViewModel = TimerViewModel(seconds: seconds, goalTime: 60)
-    }
-   
     var body: some View {
-        // MARK: - Timer View
         VStack {
             VStack {
                 Text("Calmodoro Timer")
@@ -55,7 +30,7 @@ struct ContentView: View {
                     if !isSetTimerButtonActive {
                         timerText
                     }
-                                      
+                    
                     if (isPomodoroTimerActive || isBreakTimerActive) && isSetTimerButtonActive {
                         SetTimerView()
                     }
@@ -86,58 +61,10 @@ struct ContentView: View {
             .background(.blue)
             .cornerRadius(15)
             .padding(20)
-            
-            // MARK: - Tasks View
-            VStack {
-                NavigationSplitView {
-                    List {
-                        ForEach(items) { item in
-                            NavigationLink {
-                                Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                            } label: {
-                                Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                            }
-                        }
-                        .onDelete(perform: deleteItems)
-                    }
-                    .toolbar {
-                        ToolbarItem(placement: .topBarLeading) {
-                            Text("Tasks")
-                        }
-                        
-                        ToolbarItem {
-                            Button(action: addItem) {
-                                Label("Add Item", systemImage: "plus")
-                            }
-                        }
-                        
-                        ToolbarItem(placement: .navigationBarTrailing) {
-                            EditButton()
-                        }
-                    }
-                } detail: {
-                    Text("Select an item")
-                }
-            }
-        }
-        .background(.indigo)
-    }
-    // MARK: - Private Views
-    private var timerText: some View {
-        VStack {
-            if isPomodoroTimerActive {
-                Text(pomodoroTimerViewModel.progress >= 1 ? "DONE" : pomodoroTimerViewModel.displayTime)
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
-            } else if isBreakTimerActive {
-                Text(breakTimerViewModel.progress >= 1 ? "DONE" : breakTimerViewModel.displayTime)
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
-            }
         }
     }
-   
-    // MARK: - Top Button View
+    
+    // MARK: - TopButtons View
     private var topButtons: some View {
         HStack {
             if isPaused {
@@ -173,20 +100,6 @@ struct ContentView: View {
         }
         .foregroundColor(.indigo)
     }
-   
-    // MARK: - Reset Button View
-    private var resetButton: some View {
-        Button {
-            reset(timerViewModel: isPomodoroTimerActive ? pomodoroTimerViewModel : breakTimerViewModel, rotation: rotation)
-        } label: {
-            HStack(spacing: 0) {
-                Image(systemName: "arrow.clockwise")
-                    .rotationEffect(.degrees(Double(rotation)))
-                Text("Reset")
-            }
-        }
-    }
-    
     // MARK: - StartPause Button View
     private var startPauseButton: some View {
         Button {
@@ -204,17 +117,36 @@ struct ContentView: View {
             }
         }
     }
-        
-    // MARK: - Private Methods
     
-    private func setTimer() {
-        if isPomodoroTimerActive {
-            pomodoroTimerViewModel.updateGoalTime()
-        } else if isBreakTimerActive {
-            breakTimerViewModel.updateGoalTime()
+    // MARK: - Reset Button View
+    private var resetButton: some View {
+        Button {
+            reset(timerViewModel: isPomodoroTimerActive ? pomodoroTimerViewModel : breakTimerViewModel, rotation: rotation)
+        } label: {
+            HStack(spacing: 0) {
+                Image(systemName: "arrow.clockwise")
+                    .rotationEffect(.degrees(Double(rotation)))
+                Text("Reset")
+            }
         }
     }
     
+    // MARK: - Timer Display Text View
+    private var timerText: some View {
+        VStack {
+            if isPomodoroTimerActive {
+                Text(pomodoroTimerViewModel.progress >= 1 ? "DONE" : pomodoroTimerViewModel.displayTime)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+            } else if isBreakTimerActive {
+                Text(breakTimerViewModel.progress >= 1 ? "DONE" : breakTimerViewModel.displayTime)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+            }
+        }
+    }
+    
+    // MARK: - Private Methods
     private func reset(timerViewModel: TimerViewModel, rotation: Int) {
         withAnimation(.easeInOut(duration: 0.4)) {
             self.rotation += 360
@@ -229,23 +161,11 @@ struct ContentView: View {
         }
     }
     
-    private func addItem() {
-        withAnimation {
-            let newItem = Task(timestamp: Date())
-            modelContext.insert(newItem)
+    private func setTimer() {
+        if isPomodoroTimerActive {
+            pomodoroTimerViewModel.updateGoalTime()
+        } else if isBreakTimerActive {
+            breakTimerViewModel.updateGoalTime()
         }
     }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
-}
-
-#Preview {
-    ContentView()
-        .modelContainer(for: Task.self, inMemory: true)
 }

4e343000e72f53f5f45783b9404b99afb295e36c
Jordon Bigelow <the-music-dude@hotmail.com>
2024-06-15 15:54:20 -0600

    added a todo secion in the ContentView.swif file. I will use this as a way to keep my goal chuncked out into smaller tasks so I can achieve what I want to.

--- Calmodoro Timer/ContentView.swift
+++ Calmodoro Timer/ContentView.swift
@@ -6,8 +6,13 @@
 //
 
 /*
- TODO:
- I want the tasks to link with the timers. So esentially, if you make a
+//MARK: - TODO
+-Figure out how to change the behavior of Swift Data, currently we need to know how to alter the model
+ so that we can create our own "Task" model, as well as the deatil view. I want tasks to be able to be
+ marked as complete and go to another table or something separate. The task needs to have a timer
+ to be associated with it so when a user touches one of the tasks, it loads the timer into memory.
+
+ -I want the tasks to link with the timers. So esentially, if you make a
  timer by interacting with the set timer function, and then click the plus
  button, it will create a task that you can then touch that will set the
  timer to whatever time is stored in that task. Essentially, this makes
@@ -15,6 +20,15 @@
  will promote the pomodoro method by causing you to chunk a large task
  into a smaller task you can accomplish in a small amount of time, such as
  20-40 minutes with a 5-15 minute break afterwards.
+ 
+ -Figure out a way to make it obvious that a timer is "set". Currently the user doesn't truly know if
+  the timer got set, other than checking the set timer button, but I don't think that's intuitive
+ 
+//MARK: - Active Task
+ -Shadow color on the start/pause and reset buttons is a different color compared to the top buttons shadow color
+
+ MARK: - BUGS
+ -if the user is scrolling, the timer pauses ugh...the ui interaction is hanging the thread for the timer
  */
 
 import SwiftUI
